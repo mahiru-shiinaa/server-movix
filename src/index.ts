1f2189d246c9a081c04e299870a11800f33747db
Mahiru Shiina <halovemusics@gmail.com>
2025-08-31 17:14:35 +0700

    Version 1: Khởi tạo project

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import express, { Express} from "express";
+import dotenv from "dotenv";
+// Cấu hình .env
+dotenv.config();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import * as database from "./config/database";
+
+// Khởi động app và thiết lập port
+const app: Express = express();
+const port: number | string = process.env.PORT || 3000;
+
+// Kết nối Database
+database.connect();
+
+// Middleware để đọc body từ client, không cần body-parser nâng cao
+app.use(express.json()); // Đọc JSON từ client (axios/fetch gửi lên)
+app.use(express.urlencoded({ extended: true })); // Nếu dùng form HTML gửi lên
+
+//  Cho phép CORS
+app.use(
+  cors({
+    origin: ["http://localhost:3000"], // ✅ Chỉ cho phép React app
+    credentials: true, //Cho phéo gửi request với cookie, phải có
+  })
+); //  cấu hình mặc định: cho phép tất cả origin
+
+app.use(cookieParser());
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
